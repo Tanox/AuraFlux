ddfe918f1020e6820086ce51edcc8e898d0da66f
Sut <xepinchan@qq.com>
2026-04-15 17:47:55 +0800

    fix(VisualizerCanvas): 修复类型断言错误
    build: 添加项目依赖项
    
    在VisualizerCanvas组件中修复了dataArrayR的类型断言错误，确保正确调用getByteFrequencyData方法。同时添加了项目运行和开发所需的所有依赖项到package.json中。

--- src/components/visualizers/VisualizerCanvas.tsx
+++ src/components/visualizers/VisualizerCanvas.tsx
@@ -51,9 +51,9 @@ const VisualizerCanvas: React.FC<Props> = ({ analyser, analyserR, colors, settin
     const height = canvas.height;
     initStars(width, height);
 
-    let dataArrayR: Uint8Array | undefined;
+    let dataArrayR: Uint8Array<ArrayBuffer> | undefined;
     if (analyserR) {
-      dataArrayR = new Uint8Array(analyserR.frequencyBinCount);
+      dataArrayR = new Uint8Array(analyserR.frequencyBinCount) as Uint8Array<ArrayBuffer>;
     }
 
     const draw = () => {
