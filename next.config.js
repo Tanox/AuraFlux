6cda480f45e363d3d54daa1cb0b054c429b03f3e
Sut <xepinchan@qq.com>
2026-04-16 12:00:00 +0800

    refactor: 清理未使用的配置和修复类型声明
    移除next.config.js中未使用的webpack配置和path依赖
    删除tsconfig.json中冗余的类型声明和路径映射
    修复VisualizerCanvas中错误的类型断言

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei", "postprocessing", "@react-three/postprocessing", "@google/genai"],
@@ -11,13 +9,6 @@ const nextConfig = {
   productionBrowserSourceMaps: false,
   typescript: {
     ignoreBuildErrors: true
-  },
-  webpack: (config) => {
-    config.resolve.modules = [
-      path.resolve(process.env.USERPROFILE + '\\.npm-global\\node_modules'),
-      ...config.resolve.modules
-    ];
-    return config;
   }
 };
 
