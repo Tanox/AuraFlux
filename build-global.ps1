f69ae365c60df99741a71b58e38368c3f224830f
Sut <xepinchan@qq.com>
2026-04-21 17:05:59 +0800

    chore: 更新版本号至v2.3.5并修复音频工具错误处理
    - 更新多个文件的版本号注释
    - 修改音频工具中空数组处理的错误抛出逻辑
    - 更新PWA服务工作线程配置

--- build-global.ps1
+++
@@ -1,8 +0,0 @@
-# 使用全局包构建应用
-$env:NODE_PATH = "$env:USERPROFILE\.npm-global\node_modules"
-$env:NEXT_DIST_DIR = "$env:USERPROFILE\.npm-global\.next"
-
-Write-Host "Building with global packages..."
-& "$env:USERPROFILE\.npm-global\bin\next.cmd" build --webpack
-
-Write-Host "Build completed!"
